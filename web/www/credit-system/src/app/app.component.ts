bad0ec9949addf349f5d51b841b1b3ab3c9fbb89
B326872 <tara.morin@bnsf.com>
2026-03-03 10:31:33 -0500

    set up dynamically hiding main component

--- web/www/credit-system/src/app/app.component.ts
+++ web/www/credit-system/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,17 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class AppComponent {
 
-  
+  onMainPage: boolean = true;
+
+  constructor(private router: Router){
+    this.onMainPage = this.router.url == '';
+
+    //checks for navigation end- when the navigation to a new page has finished- to see what the route is
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.onMainPage = this.router.url == '';
+      });
+  }
 
 }
